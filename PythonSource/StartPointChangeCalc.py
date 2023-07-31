caf53c1fc097ba140eff206e3d38f4ae155e9d65
1999kazukiy <kazukiy99@gmail.com>
2023-07-31 12:11:32 +0900

    Upadate

--- PythonSource/StartPointChangeCalc.py
+++ PythonSource/StartPointChangeCalc.py
@@ -2,36 +2,44 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# csvファイルの読み込み
-df = pd.read_csv('csv_file_path')
+#####################
+## 設定
+filepath = "test731.csv"     # 読み込むファイルパス
+true_values = 60 / 90 * 1000 # rmseの真値
+StartPoint = 5               # 開始列
+EndPoint = 30                # 終了列
+##########################
 
-# 初期化
+# 初期化 平均値を保存する配列
 mean_stdevs = []
 mean_rmse = []
 
+# csvファイルの読み込み
+df = pd.read_csv(filepath)
+
 # 開始列を1から31に変化させ、10列のデータを選択
-for i in range(5, 31):
+for i in range(StartPoint, EndPoint):
     stdevs = []
     rmse = []
-    # Loop over all rows
-    for index, row in df.iterrows():
+    
+    for _, row in df.iterrows(): # 1行ずつ取り出す
         data = row[i:i+10]
         stdev = data.std()
         stdevs.append(stdev)
-        rmse.append(np.sqrt(((data - 500) ** 2).mean()))
+        rmse.append(np.sqrt(((data - true_values) ** 2).mean()))
     mean_stdevs.append(np.mean(stdevs))
     mean_rmse.append(np.mean(rmse))
 
-# Plot mean standard deviations
+# 標準偏差のプロット
 plt.figure(figsize=(10, 5))
-plt.plot(range(1, 31), mean_stdevs)
+plt.plot(range(1, EndPoint), mean_stdevs)
 plt.xlabel("First index of data")
-plt.ylabel("STDEV")
+plt.ylabel("Standard deviation [ms]")
 plt.show()
 
-# Plot mean RMSE
+# RMSEのプロット
 plt.figure(figsize=(10, 5))
-plt.plot(range(1, 31), mean_rmse)
+plt.plot(range(1, EndPoint), mean_rmse)
 plt.xlabel("First index of data")
-plt.ylabel("RMSE")
+plt.ylabel("RMSE [ms]")
 plt.show()
